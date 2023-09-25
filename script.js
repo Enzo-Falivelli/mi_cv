96cc17b9a484d19c60a85db1c7825f0a4b510192
Enzo-Falivelli <142274768+Enzo-Falivelli@users.noreply.github.com>
2023-09-25 01:54:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,74 +1,127 @@
 // script.js
 
-function clearLists(){
-    var centeredLists = document.getElementsByClassName('list');
-        for(let i=0; i < centeredLists.length; i++){
-            if(!centeredLists[i].classList.contains('hidden'))
-                centeredLists[i].classList.add('hidden');
+    function clearLists(){
+        var centeredLists = document.getElementsByClassName('list');
+        hideList(centeredLists);
+    }
+
+    function hideList(list){
+        for(let i=0; i < list.length; i++){
+            if(!list[i].classList.contains('hidden'))
+                list[i].classList.add('hidden');
         }
-}
+    }
 
-document.getElementById('experience-link').addEventListener('click', function(event) {
-    event.preventDefault();
+    function closeNav(){
+        document.getElementById('menu').checked = false;
+    }
+  
 
-    var imageContainer = document.querySelector('.image-container');
-    var centeredImage = document.getElementById('centered-image');
-    var centeredList = document.getElementById('experience-list');
+    document.getElementById('experience-link').addEventListener('click', function(event) {
+        event.preventDefault();
+    
+        var imageContainer = document.querySelector('.image-container');
+        var centeredImage = document.getElementById('centered-image');
+        var centeredList = document.getElementById('experience-list');
+    
+        var centeredImage = document.getElementById('centered-image');
+        clearLists();
+        closeNav();
+        // Mueve la imagen al margen izquierdo
+        if (window.innerWidth <= 768) 
+        {
+            centeredImage.style.display = 'none';
+            
 
-    var centeredImage = document.getElementById('centered-image');
-    clearLists();
-    // Mueve la imagen al margen izquierdo
-   centeredImage.style.marginLeft = '-70%';
+        }
+        else
+        {
+        centeredImage.style.marginLeft = '-70%';
+        }
+        // Muestra la lista centrada
+        centeredList.classList.remove('hidden');
+    });
 
-    // Muestra la lista centrada
-    centeredList.classList.remove('hidden');
-});
+    // script.js
+    document.getElementById('skills-link').addEventListener('click', function(event) {
+        event.preventDefault();
+    
+        var imageContainer = document.querySelector('.image-container');
+        var centeredImage = document.getElementById('centered-image');
+        var centeredList = document.getElementById('skills-list');
+    
+        var centeredImage = document.getElementById('centered-image');
+        clearLists();
+        closeNav();
+        if (window.innerWidth <= 768) 
+        {
+            centeredImage.style.display = 'none';
+            
 
-// script.js
-document.getElementById('skills-link').addEventListener('click', function(event) {
-    event.preventDefault();
+        }
+        else
+        {
+        // Mueve la imagen al margen izquierdo
+        centeredImage.style.marginLeft = '-70%';
+        }
+        // Muestra la lista centrada
+        centeredList.classList.remove('hidden');
+    });
+    
+    document.getElementById('softwares-link').addEventListener('click', function(event) {
+        event.preventDefault();
+    
+        var imageContainer = document.querySelector('.image-container');
+        var centeredImage = document.getElementById('centered-image');
+        var centeredList = document.getElementById('softwares-list');
+    
+        var centeredImage = document.getElementById('centered-image');
+        clearLists();
+        closeNav();
+        if (window.innerWidth <= 768) 
+        {
+            centeredImage.style.display = 'none';
+            
 
-    var imageContainer = document.querySelector('.image-container');
-    var centeredImage = document.getElementById('centered-image');
-    var centeredList = document.getElementById('skills-list');
+        }
+        else
+        {
+        // Mueve la imagen al margen izquierdo
+        centeredImage.style.marginLeft = '-70%';
+        }
+        // Muestra la lista centrada
+        centeredList.classList.remove('hidden');
+    });
+    
+    document.getElementById('education-link').addEventListener('click', function(event) {
+        event.preventDefault();
+    
+        var imageContainer = document.querySelector('.image-container');
+        var centeredImage = document.getElementById('centered-image');
+        var centeredList = document.getElementById('education-list');
+    
+        var centeredImage = document.getElementById('centered-image');
+        clearLists();
+        closeNav();
+        if (window.innerWidth <= 768) 
+        {
+            centeredImage.style.display = 'none';
+            
 
-    var centeredImage = document.getElementById('centered-image');
-    clearLists();
-    // Mueve la imagen al margen izquierdo
-   centeredImage.style.marginLeft = '-70%';
+        }
+        else
+        {
+        // Mueve la imagen al margen izquierdo
+        centeredImage.style.marginLeft = '-70%';
+        }
+    
+        // Muestra la lista centrada
+        centeredList.classList.remove('hidden');
+    });
 
-    // Muestra la lista centrada
-    centeredList.classList.remove('hidden');
-});
 
-document.getElementById('softwares-link').addEventListener('click', function(event) {
-    event.preventDefault();
+    
 
-    var imageContainer = document.querySelector('.image-container');
-    var centeredImage = document.getElementById('centered-image');
-    var centeredList = document.getElementById('softwares-list');
 
-    var centeredImage = document.getElementById('centered-image');
-    clearLists();
-    // Mueve la imagen al margen izquierdo
-   centeredImage.style.marginLeft = '-70%';
 
-    // Muestra la lista centrada
-    centeredList.classList.remove('hidden');
-});
 
-document.getElementById('education-link').addEventListener('click', function(event) {
-    event.preventDefault();
-
-    var imageContainer = document.querySelector('.image-container');
-    var centeredImage = document.getElementById('centered-image');
-    var centeredList = document.getElementById('education-list');
-
-    var centeredImage = document.getElementById('centered-image');
-    clearLists();
-    // Mueve la imagen al margen izquierdo
-   centeredImage.style.marginLeft = '-70%';
-
-    // Muestra la lista centrada
-    centeredList.classList.remove('hidden');
-});
